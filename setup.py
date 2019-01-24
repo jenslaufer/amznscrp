82a115c69e61c0f6c829717ec135ec628903a54c
Jens Laufer <jenslaufer@gmail.com>
2019-01-24 08:55:18 +0100

    changed

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
-    
     name='amzsrcp',
     url='https://github.com/jenslaufer/amzsrcp',
     author='Jens Laufer',
     author_email='jenslaufer@gmail.com',
-    packages=['amznscrp'],
+    packages=['amzscrp'],
     install_requires=['urllib','re','requests','argparse','string'],
     version='0.1',
     license='MIT',
