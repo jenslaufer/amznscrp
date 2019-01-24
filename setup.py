8589755e9834136ce33692b1688862229e856949
Jens Laufer <jenslaufer@gmail.com>
2019-01-24 16:59:27 +0100

    changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='jenslaufer@gmail.com',
     packages=['amznscrp'],
     install_requires=['requests', 'argparse', 'bottlenose',
-                      'bs4', 'lxml', 'get_smarties', 'scikit-learn', 'pickle', 'pandas', 'numpy'],
+                      'bs4', 'lxml', 'get_smarties', 'scikit-learn', 'pandas', 'numpy'],
     version='0.0.1',
     license='MIT',
     description='Tools to scrp Amz',
