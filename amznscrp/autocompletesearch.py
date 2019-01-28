b4b52dd679cf276cbf75b91beaf2666abfebd918
Jens Laufer <jenslaufer@gmail.com>
2019-01-28 14:24:35 +0100

    changed header

--- amznscrp/autocompletesearch.py
+++ amznscrp/autocompletesearch.py
@@ -12,24 +12,23 @@ def scrape(keyword, proxyfile, useragentfile):
     user_agents = useragent.UserAgent(useragentfile)
 
     s = requests.session()
-    s.headers.update(
-        headers={
-            'User-Agent': user_agents.get(),
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        })
+    headers = {
+        'User-Agent': user_agents.get(),
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    }
     base_url = 'https://completion.amazon.co.uk'
     mid = 'A1PA6795UKMFR9'
     lop = 'de_DE'
     uri = '{0}/api/2017/suggestions?lop={1}&mid={2}&alias=aps&prefix={3}'
 
     f_kwrd = quote_plus(keyword)
-    result = s.get(uri.format(base_url, lop, mid, f_kwrd),
+    result = s.get(uri.format(base_url, lop, mid, f_kwrd), headers=headers,
                    proxies=proxy_srv.get())
     if "Invalid Marketplace ID" in result.text:
         resp = s.get(base_url).text
         mid = re.findall(re.compile(
             r'obfuscatedMarketId:\s"(.*)"'), resp)[0]
-        result = s.get(uri.format(mid, f_kwrd),
+        result = s.get(uri.format(mid, f_kwrd), headers=headers,
                        proxies=proxy_srv.get())
 
     return json.loads(result.content)
