10a2c475ebf203490c40ecbe9a097aab7034f1ba
Jens Laufer <jenslaufer@gmail.com>
2019-01-30 08:21:47 +0100

    more generic way

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/jenslaufer/amznscrp.git',
     author='Jens Laufer',
     author_email='jenslaufer@gmail.com',
-    packages=['amznscrp'],
+    packages=find_packages(),
     install_requires=['requests', 'argparse', 'bottlenose',
                       'bs4', 'lxml', 'get_smarties', 'scikit-learn', 'pandas', 'numpy'],
     version='0.0.1',
